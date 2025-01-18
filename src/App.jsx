67f31bb3ba85dec30e7dbc658c9686cba4bf206c
emirisato1003 <emirisato.1003@gmail.com>
2025-01-18 12:53:07 -0800

    create language chips section and styled it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import { nanoid } from 'nanoid';
+import { languages } from './languages';
 import './App.css';
 
 function App() {
 
+  const languageElement = languages.map(lang => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color
+    };
+
+    return (
+      <span className='chip' style={styles}>
+        {lang.name}
+      </span>
+    );
+  });
 
   return (
     <main>
@@ -12,9 +26,14 @@ function App() {
         <h1>Assembly: Endgame</h1>
         <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
-      <section className="status">
+
+      <section className="game-status">
         <h2>You win!</h2>
         <p>Well done!🎉</p>
+      </section>
+
+      <section className='language-chips'>
+        {languageElement}
       </section>
     </main>
   );
