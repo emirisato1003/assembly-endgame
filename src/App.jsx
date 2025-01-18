94b181cf416a3a943b03c7ae1e4f7893b6a47e07
emirisato1003 <emirisato.1003@gmail.com>
2025-01-18 11:40:26 -0800

    created status section and styled it.

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,10 @@ function App() {
         <h1>Assembly: Endgame</h1>
         <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
+      <section className="status">
+        <h2>You win!</h2>
+        <p>Well done!🎉</p>
+      </section>
     </main>
   );
 }
